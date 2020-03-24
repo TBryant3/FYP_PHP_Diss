5e8e4526bfa68f2f228e72127761526d66c26db7
Tyler <tylerbryant@hotmail.com>
2020-03-24 19:52:12 +0000

    Moved around the progress bars, amended BundleConfig.cs to minimise CSS links on each page

--- Dissertation_FYP/App_Start/BundleConfig.cs
+++ Dissertation_FYP/App_Start/BundleConfig.cs
@@ -24,7 +24,9 @@ namespace Dissertation_FYP
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/allTopicStyles.css",
+                      "~/Content/themes/base/jquery-ui.css"));
         }
     }
 }
